c6b0f8784ba2709c22d2c54a619797003ceb6485
Wyatt Baldwin <self@wyattbaldwin.com>
2022-12-03 01:50:55 -0800

    WIP - loader & docs nav

--- website/src/djangokit/website/components/Loader.tsx
+++ website/src/djangokit/website/components/Loader.tsx
@@ -1,7 +1,14 @@
+import Spinner from "react-bootstrap/Spinner";
+
 interface Props {
   children: any;
 }
 
 export default function Loader({ children }: Props) {
-  return <div>{children}</div>;
+  return (
+    <div className="d-flex flex-column align-items-center gap-4">
+      <Spinner />
+      {children}
+    </div>
+  );
 }
--- website/src/djangokit/website/routes/docs/layout.tsx
+++ website/src/djangokit/website/routes/docs/layout.tsx
@@ -2,6 +2,7 @@ import { Outlet, useLocation } from "react-router-dom";
 
 import LinkContainer from "react-router-bootstrap/LinkContainer";
 import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 import { useApiQuery } from "../../api";
 import { Page } from "../../models";
@@ -23,19 +24,31 @@ export default function Layout() {
 
   return (
     <div className="d-flex flex-column flex-md-row gap-4 h-100">
-      <Nav
-        variant="pills"
-        className="flex-row flex-md-column"
-        activeKey={location.pathname}
+      <Navbar
+        bg="dark"
+        variant="dark"
+        expand="md"
+        collapseOnSelect
+        className="flex-md-column rounded p-3 p-md-4"
       >
-        {data.pages.map((page) => (
-          <Nav.Item key={page.id}>
-            <LinkContainer to={`/docs/${page.slug.slice(4)}`}>
-              <Nav.Link>{page.title}</Nav.Link>
-            </LinkContainer>
-          </Nav.Item>
-        ))}
-      </Nav>
+        <Navbar.Toggle aria-controls="nav-docs" className="d-md-none w-100">
+          <div className="d-flex align-items-center gap-2">
+            <span className="navbar-toggler-icon"></span>
+            <span>Docs Menu</span>
+          </div>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="nav-docs" className="flex-column">
+          <Nav className="flex-column" activeKey={location.pathname}>
+            {data.pages.map((page) => (
+              <Nav.Item key={page.id}>
+                <LinkContainer to={`/docs/${page.slug.slice(4)}`}>
+                  <Nav.Link>{page.title}</Nav.Link>
+                </LinkContainer>
+              </Nav.Item>
+            ))}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
 
       <div className="flex-fill">
         <Outlet />
