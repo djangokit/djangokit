5cc0007db6eccf6471e887b6e6453cc52720047e
Wyatt Baldwin <self@wyattbaldwin.com>
2022-12-03 18:10:44 -0800

    WIP - Add command to clean up old versions on remote/deployment host

--- website/commands.py
+++ website/commands.py
@@ -1,8 +1,10 @@
 import os
+import re
 
 from djangokit.core.env import load_dotenv
-from runcommands import arg, command
+from runcommands import abort, arg, command
 from runcommands import commands as c
+from runcommands.commands import remote
 from runcommands import printer
 
 
@@ -33,7 +35,7 @@ def clean(deep=False):
 
 
 @command
-def ansible(env, hostname, version=None, tags=(), skip_tags=(), extra_vars=()):
+def ansible(env, host, version=None, tags=(), skip_tags=(), extra_vars=()):
     """Run ansible playbook."""
     version = version or c.git_version()
 
@@ -60,7 +62,7 @@ def ansible(env, hostname, version=None, tags=(), skip_tags=(), extra_vars=()):
             skip_tags,
             extra_vars,
             ("--extra-var", f"env={env}"),
-            ("--extra-var", f"hostname={hostname}"),
+            ("--extra-var", f"hostname={host}"),
             ("--extra-var", f"version={version}"),
         ),
         cd=os.path.dirname(__file__),
@@ -68,29 +70,29 @@ def ansible(env, hostname, version=None, tags=(), skip_tags=(), extra_vars=()):
 
 
 @command
-def provision(env, hostname):
+def provision(env, host):
     """Provision the deployment host."""
-    printer.header(f"Provisioning {hostname} ({env})")
-    ansible(env, hostname, tags="provision")
+    printer.header(f"Provisioning {host} ({env})")
+    ansible(env, host, tags="provision")
 
 
 @command
-def upgrade_remote(env, hostname):
+def upgrade_remote(env, host):
     """Upgrade the deployment host."""
-    printer.header(f"Upgrading {hostname} ({env})")
-    ansible(env, hostname, tags="provision-update-packages")
+    printer.header(f"Upgrading {host} ({env})")
+    ansible(env, host, tags="provision-update-packages")
 
 
 @command
 def prepare(
     env,
-    hostname,
+    host,
     version=None,
     provision_=False,
     clean_: arg(help="Remove build directory? [no]") = False,
 ):
     """Prepare build locally for deployment."""
-    printer.header(f"Preparing deployment to {hostname} ({env})")
+    printer.header(f"Preparing deployment to {host} ({env})")
     if clean_:
         c.local("rm -rf build")
     version = version or c.git_version()
@@ -99,13 +101,13 @@ def prepare(
         tags.append("provision")
     tags.append("prepare")
     printer.hr(f"Preparing DjangoKit website version {version} for deployment")
-    ansible(env, hostname, tags=tags, extra_vars={"version": version})
+    ansible(env, host, tags=tags, extra_vars={"version": version})
 
 
 @command
 def deploy(
     env: arg(help="Build/deployment environment"),
-    hostname: arg(help="Host to deploy to"),
+    host: arg(help="Host to deploy to"),
     version: arg(help="Name of version being deployed [short git hash]") = None,
     provision_: arg(help="Run provisioning steps? [no]") = False,
     prepare_: arg(
@@ -125,9 +127,9 @@ def deploy(
     bool_as_str = lambda b: "yes" if b else "no"
 
     printer.hr()
-    printer.header(f"Deploying to {hostname} ({env})\n")
+    printer.header(f"Deploying to {host} ({env})\n")
     printer.print(f"env = {env}")
-    printer.print(f"hostname = {hostname}")
+    printer.print(f"host = {host}")
     printer.print(f"version = {version}")
     printer.print(f"provision = {bool_as_str(provision_)}")
     printer.print(f"local prep = {bool_as_str(prepare_)}")
@@ -150,4 +152,55 @@ def deploy(
     tags.append("deploy")
 
     printer.hr(f"Deploying DjangoKit website version {version}")
-    ansible(env, hostname, version, tags=tags, skip_tags=skip_tags)
+    ansible(env, host, version, tags=tags, skip_tags=skip_tags)
+
+
+@command
+def clean_remote(root="/sites/djangokit.org", run_as="djangokit"):
+    printer.header(f"Removing old versions from {root}")
+
+    readlink_result = remote(
+        f"readlink current",
+        run_as=run_as,
+        cd=root,
+        stdout="capture",
+    )
+
+    current_path = readlink_result.stdout.strip()
+
+    if not current_path:
+        abort(404, f"Could not read current link in {root}")
+
+    current_version = os.path.basename(current_path)
+    printer.success(f"Current version: {current_version}")
+
+    find_result = remote(
+        f"find {root} -mindepth 1 -maxdepth 1 -type d -not -name '.*' -not -name 'pip'",
+        run_as=run_as,
+        stdout="capture",
+    )
+
+    paths = find_result.stdout_lines
+    paths = [p for p in paths if re.fullmatch(r"[0-9a-f]{12}", os.path.basename(p))]
+
+    if not paths:
+        abort(404, f"No versions found in {root}")
+
+    try:
+        paths.remove(current_path)
+    except ValueError:
+        paths = "\n".join(paths)
+        abort(404, f"Current version not found in paths:\n{paths}")
+
+    if not paths:
+        abort(0, f"No versions other than current found", color="warning")
+
+    for path in paths:
+        version = os.path.basename(path)
+
+        du_result = remote(f"du -sh {path} | awk '{{ print $1 }}'", stdout="capture")
+        size = du_result.stdout.strip()
+
+        printer.warning(f"Removing version: {version} ({size})... ", end="")
+        remote(f"rm -r {path}", run_as=run_as)
+        printer.success("Done")
