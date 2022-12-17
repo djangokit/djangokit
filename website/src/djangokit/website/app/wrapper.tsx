944abd5b758012f86b4b3df1ba7f09620da6b0a8
Wyatt Baldwin <self@wyattbaldwin.com>
2022-12-17 06:01:36 -0800

    website: Wrap app in SSRProvider

--- website/src/djangokit/website/app/wrapper.tsx
+++ website/src/djangokit/website/app/wrapper.tsx
@@ -1,9 +1,12 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import SSRProvider from "react-bootstrap/SSRProvider";
 
 const queryClient = new QueryClient();
 
 export default function Wrapper({ children }) {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <SSRProvider>{children}</SSRProvider>
+    </QueryClientProvider>
   );
 }
