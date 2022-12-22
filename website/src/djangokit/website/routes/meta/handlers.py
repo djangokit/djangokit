d8f5451cc166437f262ad0691a0f6ba3baeb1ff2
Wyatt Baldwin <self@wyattbaldwin.com>
2022-12-22 04:29:44 -0800

    website: Include *all* settings on meta page for superusers
    Use Django's utilities for scrubbing sensitive settings

--- website/src/djangokit/website/routes/meta/handlers.py
+++ website/src/djangokit/website/routes/meta/handlers.py
@@ -1,32 +1,27 @@
 import os
 
 from django.conf import settings
-
-INCLUDE_SETTINGS = (
-    "DEBUG",
-    "ALLOWED_HOSTS",
-    "ROOT_URLCONF",
-    "WSGI_APPLICATION",
-    "TIME_ZONE",
-    "LOGIN_URL",
-    "STATIC_ROOT",
-    "STATIC_URL",
-    "STATICFILES_STORAGE",
-)
+from django.views.debug import SafeExceptionReporterFilter
 
 
 def get(request):
+    safe_filter = SafeExceptionReporterFilter()
+
     data = {
         "env": settings.ENV,
         "version": os.getenv("VERSION"),
     }
 
     if request.user.is_superuser:
+        django_settings = safe_filter.get_safe_settings()
+        django_settings.pop("DJANGOKIT", None)
+
         dk_settings = settings.DJANGOKIT.as_dict()
         for n, v in dk_settings.items():
             if callable(v):
                 dk_settings[n] = f"{v.__module__}.{v.__name__}"
-        data["settings"] = {n: getattr(settings, n) for n in INCLUDE_SETTINGS}
+
+        data["settings"] = django_settings
         data["settings"]["DJANGOKIT"] = dk_settings
 
     return data
