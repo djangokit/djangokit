0da56b75fcfd298b06d93d0487378e63b1674595
Wyatt Baldwin <self@wyattbaldwin.com>
2022-12-25 07:30:03 -0800

    core: Simplify middleware tests
    Use pytest fixtures and disuse SimpleTestCase

--- core/tests/test_middleware.py
+++ core/tests/test_middleware.py
@@ -1,27 +1,28 @@
-from django.test import Client, SimpleTestCase
+import pytest
 
 from djangokit.core.http import make_request
 from djangokit.core.middleware import djangokit_middleware
 
 
-class TestMiddleware(SimpleTestCase):
-    def setUp(self):
-        self.middleware = djangokit_middleware(lambda req: req)
-        self.client = Client()
+@pytest.fixture
+def middleware():
+    return djangokit_middleware(lambda req: req)
 
-    def test_json_ext(self):
-        path = "/docs/test.json"
-        request = make_request(path=path)
-        result = self.middleware(request)
-        self.assertEqual(result.path, "/docs/test")
-        self.assertEqual(result.path_info, "/docs/test")
-        self.assertEqual(result.META["PATH_INFO"], "/docs/test")
-        self.assertEqual(result.headers["Accept"], "application/json")
 
-    def test_json_ext_via_client(self):
-        path = "/docs/test.json"
-        response = self.client.get(path)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.headers["Content-Type"], "application/json")
-        data = response.json()
-        self.assertEqual(data, {"slug": "test"})
+def test_json_ext(middleware):
+    path = "/docs/test.json"
+    request = make_request(path=path)
+    result = middleware(request)
+    assert result.path == "/docs/test"
+    assert result.path_info == "/docs/test"
+    assert result.META["PATH_INFO"] == "/docs/test"
+    assert result.headers["Accept"] == "application/json"
+
+
+def test_json_ext_via_client(client):
+    path = "/docs/test.json"
+    response = client.get(path)
+    assert response.status_code == 200
+    assert response.headers["Content-Type"] == "application/json"
+    data = response.json()
+    assert data == {"slug": "test"}
