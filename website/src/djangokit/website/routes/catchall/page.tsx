9822f523169babd291b9fb7a796550d084159d72
Wyatt Baldwin <self@wyattbaldwin.com>
2022-12-25 04:54:36 -0800

    website: Add reusable NotFound component
    There are two 404 Not Found scenarios that need to be handled:
    
    1. A matching route exists but an API call returns a 404. This is
       handled however the page for the route happens to handle it.
    
    2. A matching route does not exist. This is handled by the catchall
       route.

--- website/src/djangokit/website/routes/catchall/page.tsx
+++ website/src/djangokit/website/routes/catchall/page.tsx
@@ -1,17 +1,7 @@
 /* Catchall Page (default 404 page) */
 
-import { Link } from "react-router-dom";
+import NotFound from "../../components/NotFound";
 
 export default function Page() {
-  return (
-    <div className="p-4 border border-danger rounded">
-      <h1 className="text-danger">⛔️ Not Found</h1>
-
-      <p className="lead">That page wasn&apos;t found :(</p>
-
-      <p>
-        Please re-check the address or visit our <Link to="/">home page</Link>.
-      </p>
-    </div>
-  );
+  return <NotFound />;
 }
