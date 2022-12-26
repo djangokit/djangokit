fd4dd8ebb8f1c12c71bc26164640fe8021ac1d9d
Wyatt Baldwin <self@wyattbaldwin.com>
2022-12-26 11:02:07 -0800

    website: Add edit link to individual doc pages
    Docs are edited a lot and this makes it a lot easier to get to the
    Django Admin edit page for a doc when reading/reviewing it.

--- website/src/djangokit/website/routes/docs/_slug/page.tsx
+++ website/src/djangokit/website/routes/docs/_slug/page.tsx
@@ -1,10 +1,12 @@
 import { useParams } from "react-router-dom";
+import { FaEdit } from "react-icons/fa";
 
 import { usePageQuery } from "../../../api";
 import ErrorMessage from "../../../components/ErrorMessage";
 import Loader from "../../../components/Loader";
 
 export default function Page() {
+  const currentUser = useCurrentUserContext();
   const { slug } = useParams();
   const { isLoading, isError, data, error } = usePageQuery(`doc-${slug}`);
 
@@ -19,6 +21,19 @@ export default function Page() {
   return (
     <>
       <h2>{data.title}</h2>
+
+      {currentUser.isSuperuser ? (
+        <a
+          title="Edit doc in Django Admin"
+          href={`/$admin/djangokit_website/page/${data.id}/change/`}
+          target="djangokit_admin"
+          className="d-flex align-items-center gap-2"
+        >
+          <FaEdit />
+          Edit Page
+        </a>
+      ) : null}
+
       {data.lead ? (
         <div
           dangerouslySetInnerHTML={{ __html: data.lead }}
