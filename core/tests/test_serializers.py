34aaa000f3b62877e17b2f40494afa9e150208b0
Wyatt Baldwin <self@wyattbaldwin.com>
2023-02-04 12:33:22 -0800

    core: Fix dump_json()
    `dump_json()` is a partial wrapping `json.dumps()` that sets the encoder
    class to DjangoKit's `JsonEncoder`.
    
    Previously, it was inadvertently using Django's JSON encoder class.
    
    This also disables doctesting since it causes conflicts when importing
    models from the test app. This should be okay since there are no
    doctests anyway.

---
+++ core/tests/test_serializers.py
@@ -0,0 +1,42 @@
+import pytest
+from djangokit.core.serializers import dump_json
+from djangokit.core.test.models.page import Page
+
+
+def test_dump_model_instance():
+    dump_json(Page(title="Page 1", slug="page-1", content="Content 1"))
+
+
+@pytest.mark.django_db
+def test_dump_queryset():
+    dump_json(Page.objects.all())
+
+
+def test_dump_dict_with_model_instances():
+    dump_json(
+        {
+            "1": Page(title="Page 1", slug="page-1", content="Content 1"),
+            "2": Page(title="Page 2", slug="page-2", content="Content 2"),
+            "3": Page(title="Page 3", slug="page-3", content="Content 3"),
+            "pages": [
+                Page(title="Page 1", slug="page-1", content="Content 1"),
+                Page(title="Page 2", slug="page-2", content="Content 2"),
+                Page(title="Page 3", slug="page-3", content="Content 3"),
+            ],
+        }
+    )
+
+
+def test_dump_list_of_model_instances():
+    dump_json(
+        [
+            Page(title="Page 1", slug="page-1", content="Content 1"),
+            Page(title="Page 2", slug="page-2", content="Content 2"),
+            Page(title="Page 3", slug="page-3", content="Content 3"),
+            {
+                "1": Page(title="Page 1", slug="page-1", content="Content 1"),
+                "2": Page(title="Page 2", slug="page-2", content="Content 2"),
+                "3": Page(title="Page 3", slug="page-3", content="Content 3"),
+            },
+        ]
+    )
